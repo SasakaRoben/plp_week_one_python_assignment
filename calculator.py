eb410ac0fb290d6e43e718a8912e44515b3195fb
SasakaRoben <sasakarobenld@gmail.com>
2025-08-04 00:16:47 +0300

    Perform the operation based on the user's input

--- calculator.py
+++ calculator.py
@@ -1,3 +1,18 @@
-number1 = int(input("Enter the first number"))
-number2 = int(input("Enter the second number"))
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
 operator = input("Enter the operator (+, -, *, /): ")
+
+if operator == '+':
+    result = number1 + number2
+elif operator == '-':
+    result = number1 - number2
+elif operator == '*':
+    result = number1 * number2
+elif operator == '/':
+    if number2 != 0:
+        result = number1 / number2
+    else:
+        result = "Error: Division by zero is not allowed."
+else:
+    result = "Error: Invalid operator."
+
